b9e999af0e6c813d84751eb6731d9f6fd55c904c
Tobias Wintrich <twintrich@rangee.com>
2025-10-29 09:33:23 +0100

    init

---
+++ src/Processors/Processors.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Stevebauman\Wmi\Processors;
+
+use Stevebauman\Wmi\Models\Variants\Processor;
+use Stevebauman\Wmi\Schemas\Classes;
+
+class Processors extends AbstractProcessor
+{
+    /**
+     * Returns all processors on the computer.
+     *
+     * @return array
+     */
+    public function get()
+    {
+        $processors = [];
+
+        $result = $this->connection->newQuery()->from(Classes::WIN32_PROCESSOR)->get();
+
+        foreach($result as $processor) {
+            $processors[] = new Processor($processor);
+        }
+
+        return $processors;
+    }
+}
